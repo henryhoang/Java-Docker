67b491134865d0498d63dd9e5af94b6d9bc01be1
henryhoang <henry.hoang@j2.com>
2014-10-29 14:30:32 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /root/Java-Docker
+cd /root/Java-Docker  #from local host
 
 javac GuessBirthDateUsingConfirmationDialog.java  #update java app name to execute
 java GuessBirthDateUsingConfirmationDialog
